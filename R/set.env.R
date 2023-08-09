53c25e2083f33a39702f3d985c16053f80135f4c
Tim <timjp@unimelb.edu.au>
2023-08-09 17:21:56 +1000

    Fixed to work without SAGA

--- R/set.env.R
+++ R/set.env.R
@@ -1,14 +1,14 @@
 #'
 #' @export
-set.env <- function(working.path=getwd(), saga.path='', saga.modules='', saga.lib.prefix='') {
+set.env <- function(working.path=getwd(), saga.path=NULL, saga.modules=NULL, saga.lib.prefix='') {
 
   # Check RSAGA is loaded
   if (!require('RSAGA',quietly = T))
     stop('Please install the RSAGA package. It is required by HydroMap.')
   
-  if (!exists('pkg.env'))
-    pkg.env <<- new.env()
+  pkg.env <<- new.env()
 
+  pkg.env$DEM <<- NULL
   pkg.env$DEM.data <<- NULL
   pkg.env$MrVBF.data <<- NULL
   pkg.env$MrVBF.grid <<- NULL
@@ -26,18 +26,20 @@ set.env <- function(working.path=getwd(), saga.path='', saga.modules='', saga.li
   # Set working folder
   pkg.env$working.path <<- working.path;
 
-  if ('RSAGA' %in% installed.packages()) {
-    # Setup paths to saga_cmd and saga modules
-    pkg.env$saga.settings <<- rsaga.env(path=saga.path,modules=saga.modules,lib.prefix=saga.lib.prefix,workspace=pkg.env$working.path)
-
-    # Check Mr VBF module is available
-    pkg.env$saga.has.MrVFB <<- rsaga.module.exists(lib = 'ta_morphometry', module='Multiresolution Index of Valley Bottom Flatness (MRVBF)', env = pkg.env$saga.settings);
-
-
-    if (!pkg.env$saga.has.MrVFB) {
-      message('RSAGA package does not appear to setup correctly. MrVBF and MrRTF based mapping will be unavailable.')
+  if (!is.null(saga.path) && !is.null(saga.modules)) {
+    if ('RSAGA' %in% installed.packages()) {
+      # Setup paths to saga_cmd and saga modules
+      pkg.env$saga.settings <<- rsaga.env(path=saga.path,modules=saga.modules,lib.prefix=saga.lib.prefix,workspace=pkg.env$working.path)
+      
+      # Check Mr VBF module is available
+      pkg.env$saga.has.MrVFB <<- rsaga.module.exists(lib = 'ta_morphometry', module='Multiresolution Index of Valley Bottom Flatness (MRVBF)', env = pkg.env$saga.settings);
+      
+      
+      if (!pkg.env$saga.has.MrVFB) {
+        message('RSAGA package does not appear to setup correctly. MrVBF and MrRTF based mapping will be unavailable.')
+      }
     }
   } else {
-    message('RSAGA package is not installed. Calculation of MrVBF and MrRTF grids will be unavailable.')
+      message('RSAGA package is not installed. Calculation of MrVBF and MrRTF grids will be unavailable.')
   }
 }
